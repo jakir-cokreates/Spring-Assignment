91fbf680717a241de8b99fa296a1bb3bfabe59c0
jakir-cokreates <jakir@cokreates.com>
2019-03-09 10:06:06 +0600

    fourth assignment complete

---
+++ spring-demo-annotations/src/com/jakir/TennisCoach.java
@@ -0,0 +1,14 @@
+package com.jakir;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
